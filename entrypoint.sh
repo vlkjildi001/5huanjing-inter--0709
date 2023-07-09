55ffc5b5fbe601eb17728b7a525c4a94901a173f
vlkjildi001 <106436614+vlkjildi001@users.noreply.github.com>
2023-07-09 03:43:27 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,8 @@ KOYEB_ACCOUNT=
 KOYEB_PASSWORD=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"02609b3cd6aa20ec942214de795138e6","TunnelSecret":"ZdoXN6Qu0SrL0WO/VQIYlGbFIG/W35QDzKT9tefpedU=","TunnelID":"a15d6f6d-9d6c-4b7c-98b6-a0fbcc2d2d21"}'
+ARGO_DOMAIN=inter-0709.gouweyfgywe.cf
 
 generate_config() {
   cat > config.json << EOF
